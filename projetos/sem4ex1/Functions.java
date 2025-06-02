6dfb90ee2ed7022113057becf693e500f9a53d3b
guch4n <guch4n@hotmail.com>
2025-06-02 19:04:42 -0300

    Atualização nas funções

--- projetos/sem4ex1/Functions.java
+++ projetos/sem4ex1/Functions.java
@@ -6,53 +6,17 @@ import java.util.Scanner;
 public class Functions extends Collections{
 	
 
+	
 	boolean verificadorVitoria = false;
 	Random gerador = new Random();
 	Scanner userInput = new Scanner(System.in);
 	int posicaoArray;
 	int tentativas = palavraSecreta[posicaoArray].length() + 1;
 	char escolha = 0;
-	
-	
-	public void interfaceJogo() {
-		
-		randomizadoreConversorPalavraSecreta();
-		
-		do {
-			System.out.println("Palavra: " + Mapeamento);
-			
-			System.out.println("Tentativas restantes: " + tentativas);
-			System.out.print("Digite uma letra: ");
 
-			
-			escolha = userInput.next().toLowerCase().charAt(0);
-
-			
-			if(Mapeamento.contains(escolha) && ListaCharacter.contains(escolha)){
-				System.out.println("Essa letra já foi utilizada, tente novamente");
-				
-			}else if(ListaCharacter.contains(escolha)){
-				System.out.println("Letra correta!");
-				
-			}else {
-				System.out.println("Letra incorreta!");
-				tentativas--;
-			}
-				
-			setarLetra();
-			
-			if(ListaCharacter.containsAll(Mapeamento)){
-				verificadorVitoria = true;
-				break;
-			}
-				
-		}while(tentativas > 0 || verificadorVitoria == true);
-		
-		verificarVitoria();
-	}
 	
 	
-	private void randomizadoreConversorPalavraSecreta() {
+	public void randomizadoreConversorPalavraSecreta() {
 		
 			posicaoArray = gerador.nextInt(0, 4);
 		
@@ -64,7 +28,7 @@ public class Functions extends Collections{
 	}
 	
 	
-	private void setarLetra() {
+	public void setarLetra() {
 		for (int i = 0; i < ListaCharacter.size(); i++) {
 			if (ListaCharacter.get(i) == escolha) {
 				Mapeamento.set(i, escolha);
@@ -74,7 +38,7 @@ public class Functions extends Collections{
 	}
 	
 	
-	private void verificarVitoria() {
+	public void verificarVitoria() {
 		
 		if(tentativas == 0) {
 			System.out.println("Você PERDEU!");
--- projetos/sem4ex1/Principal.java
+++ projetos/sem4ex1/Principal.java
@@ -1,13 +1,47 @@
 package sem4ex1;
 
-public class Principal {
+public class Principal extends Functions{
 
 	public static void main(String[] args) {
-
 		
 		Functions funcoes = new Functions();		
+		
+		
+			funcoes.randomizadoreConversorPalavraSecreta();
+		
+		do {
+			System.out.println("Palavra: " + funcoes.Mapeamento);
 			
-		funcoes.interfaceJogo();				
+			System.out.println("Tentativas restantes: " + funcoes.tentativas);
+			System.out.print("Digite uma letra: ");
+
+			
+			funcoes.escolha = funcoes.userInput.next().toLowerCase().charAt(0);
+
+			
+			if(funcoes.Mapeamento.contains(funcoes.escolha) && funcoes.ListaCharacter.contains(funcoes.escolha)){
+				System.out.println("Essa letra já foi utilizada, tente novamente");
+				
+			}else if(funcoes.ListaCharacter.contains(funcoes.escolha)){
+				System.out.println("Letra correta!");
+				
+			}else {
+				System.out.println("Letra incorreta!");
+				funcoes.tentativas--;
+			}
+				
+			funcoes.setarLetra();
+			
+			if(funcoes.ListaCharacter.containsAll(funcoes.Mapeamento)){
+				funcoes.verificadorVitoria = true;
+				break;
+			}
+				
+		}while(funcoes.tentativas > 0 || funcoes.verificadorVitoria == true);
+		
+		funcoes.verificarVitoria();
+		
+		
 	}
 }
 
